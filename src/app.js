9e1fde79badb79d4bdde9d40e226305e5581fc83
Nilesh Patidar <nileshpatidar875@gmail.com>
2020-12-06 22:59:39 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
 
 const express = require('express');
 const app = express()
-
+const subscriberModel=require('./models/subscribers');
 
 // Your code goes here
-
+app.get("/subscribers",async (req,res)=>{
+	const allSubscribers=await subscriberModel.find();
+	res.writeHead(200,{
+		"content-type":"application/json"
+	});
+	res.write(JSON.stringify(allSubscribers));
+	res.end();
+});
+app.get("/subscribers/names",async (req,res)=>{
+	const allSubscribers=await subscriberModel.find();
+	const output=allSubscribers.map((item,index)=>{return {"name":item.name,"subscribedChannel": item.subscribedChannel}});
+	res.writeHead(200,{
+		"content-type":"application/json"
+	});
+	res.write(JSON.stringify(output));
+	res.end();	
+});
+app.get("/subscribers/:id",async (req,res)=>{
+	try {
+	const id=req.params.id;
+	const subscriber=await subscriberModel.findById(id);
+	res.writeHead(200,{
+		"content-type":"application/json"
+	});
+	res.write(JSON.stringify(subscriber));	
+	}catch(err) {
+		res.writeHead(400,{
+		"content-type":"application/json"			
+		});
+		res.write(JSON.stringify({message: err.message}));
+	}
+	res.end();
+});
 
 
 
